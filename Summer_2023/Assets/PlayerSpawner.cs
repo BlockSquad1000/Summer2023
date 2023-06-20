72f6b5aa95c1c2239a181e9d348ff8dcfd43eeca
BlockSquad10000 <73841462+BlockSquad10000@users.noreply.github.com>
2023-06-19 22:52:06 -0400

    Worked on setting up player spawner upon entering game

---
+++ Summer_2023/Assets/PlayerSpawner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+using Photon.Pun;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject[] characterPrefabs;
+    public Transform[] startPositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PhotonNetwork.IsConnectedAndReady)
+        {
+            if(PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(GameConstants.PLAYER_SELECTION, out object playerSelection))
+            {
+                if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsValue(GameMode.DeathMatch.ToString()))
+                {
+                    int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
+                    PhotonNetwork.Instantiate(characterPrefabs[(int)playerSelection].name, startPositions[actorNumber - 1].position, Quaternion.identity);
+                }
+                else if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsValue(GameMode.CaptureTheFlag.ToString()))
+                {
+                    int randomPosition = Random.Range(-35, 35);
+                    PhotonNetwork.Instantiate(characterPrefabs[(int)playerSelection].name, new Vector3(randomPosition, 0f, randomPosition), Quaternion.identity);
+                }
+            }          
+        }
+    }
+}
